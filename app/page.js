07704843a91fff85bcdf676a40cc1519f27bfecf
aronn daniel <aronn.daniel@etudiant.univ-rennes1.fr>
2024-04-02 13:52:48 +0200

    update

--- app/page.js
+++ app/page.js
@@ -1,17 +1,25 @@
-
+// Fonction asynchrone pour effectuer une requête HTTP GET
 async function getResponse() {
+	// Effectuer une requête GET vers l'URL spécifiée avec les en-têtes requis
 	const response = await fetch(
-		'http://localhost:5000/user',
+		'http://localhost:5000/user', // URL de l'API
 		{
-			method: 'GET',
+			method: 'GET', // Méthode de requête HTTP
 			headers: {
-				'x-rapidapi-host': 'carbonfootprint1.p.rapidapi.com',
-				'x-rapidapi-key': 'your_api_key'
+				'x-rapidapi-host': 'carbonfootprint1.p.rapidapi.com', // Hôte de l'API
+				'x-rapidapi-key': 'your_api_key' // Clé d'API (à remplacer par votre propre clé)
 			}
 		}
 	);
+
+	// Vérifier si la réponse est valide (code de statut 200-299)
 	if (!response.ok) {
-		throw new Error(`HTTP error! status: ${response.status}`);
+		throw new Error(`HTTP error! status: ${response.status}`); // Lancer une erreur avec le code de statut HTTP
 	}
+
+	// Extraire les données JSON de la réponse
 	const data = await response.json();
+
+	// Retourner les données (vous pouvez choisir de retourner ou non les données selon votre besoin)
+	return data;
 }
