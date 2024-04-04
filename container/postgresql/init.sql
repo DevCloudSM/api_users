8fbf78505af68048e61bdc8da7477ed4711a1632
BobLeClou <121858449+BobLeClou@users.noreply.github.com>
2024-04-04 08:58:25 +0200

    Add files via upload

--- container/postgresql/init.sql
+++ container/postgresql/init.sql
@@ -7,13 +7,13 @@ CREATE DATABASE user_db;
 -- Crée la table des utilisateurs s'ils n'existent pas déjà
 CREATE TABLE IF NOT EXISTS users (
     ID SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL
 );
 
 -- Insère des données initiales dans la table des utilisateurs
-INSERT INTO users (username, name, surname, email) VALUES
-    ('john_doe', 'John', 'Doe', 'john@example.com'),
-    ('jane_smith', 'Jane', 'Smith', 'jane@example.com');
+INSERT INTO users (name, surname, username, email) VALUES
+    ('Doe', 'John', 'John_Doe', 'john@example.com'),
+    ('Smith', 'Jane', 'Jane_Smith', 'jane@example.com');
